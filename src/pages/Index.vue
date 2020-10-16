3bb758d71cee36bf984324320c7131fbb59f2b03
joey <hiddenk@protonmail.com>
2020-10-15 23:40:57 -0300

    feat: * Lançando page para testes

---
+++ src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+      <div class="q-pa-sm">
+
+  </div>
+</template>
+
+<script>
+
+</script>
+<style scoped>
+  :root{
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 24px;
+  }
+</style>
